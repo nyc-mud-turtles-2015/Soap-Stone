5a977d9b95c6d5e4fb8b34cbc2ebf2b6710e39c7
jkrth617 <jkrth617@gmail.com>
2015-10-29 15:14:31 -0400

    added validations and associations to follow

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,14 @@
 class Follow < ActiveRecord::Base
+	belongs_to :follower, class_name: "User", foreign_key: "follower_id"
+	belongs_to :followee, class_name: "User", foreign_key: "followee_id"
+	validates :follower, :followee, presence: true
+	validates :follower, uniqueness: {scope: :followee, message: "you have already followed this person"}
+	validate :no_following_self
+
+	def no_following_self
+		if follower == followee
+			errors.add(:follow, "you are not allowed to follow yourself")
+		end
+	end	
+
 end
