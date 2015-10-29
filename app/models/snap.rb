01aef5669f75a20dee323ff1b93cfd044d143a52
jkrth617 <jkrth617@gmail.com>
2015-10-29 15:15:54 -0400

    added associations and validations

--- app/models/snap.rb
+++ app/models/snap.rb
@@ -1,2 +1,6 @@
 class Snap < ActiveRecord::Base
+  belongs_to :drop, counter_cache: true
+  belongs_to :user
+  validates :drop, :user, presence: true
+  validates :drop, uniqueness: {scope: :user, message: "you have already snapped dis piece"}  
 end
