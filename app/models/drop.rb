f0896c36a46d731d0480b59cee27525c870d6674
jkrth617 <jkrth617@gmail.com>
2015-10-29 15:13:50 -0400

    added validations and associations to drop

--- app/models/drop.rb
+++ app/models/drop.rb
@@ -1,2 +1,14 @@
 class Drop < ActiveRecord::Base
+  belongs_to :user
+  has_many :comments
+  has_many :snaps
+  validates :user, presence: true
+  validate :has_some_content
+
+  def has_some_content
+    unless photo || text
+      errors.add(:drop, "you need something in here to drop some sick art")
+    end
+  end
+
 end
