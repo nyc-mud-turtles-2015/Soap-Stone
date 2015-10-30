7907ee82dede7a6581dc2a098ac63dcd4310800a
Peter Rosal <peter.rosal@gmail.com>
2015-10-30 12:22:40 -0400

    Remove params

--- app/controllers/drops_controller.rb
+++ app/controllers/drops_controller.rb
@@ -4,9 +4,5 @@ class DropsController < ApplicationController
     drops = Drop.order("created_at DESC").limit(50).to_json
     render json: drops
   end
-
-  def drop_params
-    params.require(:drop).permit(:photo)
-  end
   
 end
