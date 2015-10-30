c647fa4b84bec15076f2f7ddbdc5aa920de3017a
walsh9 <walsh9@gmail.com>
2015-10-30 11:51:04 -0400

    add drop create controller/route

--- app/controllers/drops_controller.rb
+++ app/controllers/drops_controller.rb
@@ -1,8 +1,24 @@
+
 class DropsController < ApplicationController
 
   def show
     drops = Drop.order("created_at DESC").limit(50).to_json
     render json: drops
   end
-  
+
+  def create
+    current_user = User.first #delete this when we have a real herper
+    drop = current_user.drops.new(drop_params)
+    drop.text = "Fake text" #delete this when we have real drop content
+    if drop.save
+      render plain: {success: true}.to_json
+    else
+      render plain: {failure: drop.errors.full_messages.join(",")}.to_json, status: 500
+    end
+  end
+
+  def drop_params 
+    params.require(:drop).permit(:photo, :text)
+      .merge(lonlat: Drop.create_lonlat(params[:drop][:coords]))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'maps#index'
 
   resources :maps, only: [:index]
-  resources :drops, only: [:show]
-  
+  resources :drops, only: [:show, :create]
+
   get "/login", :to => 'sessions#new', :as => 'login'
   get "/auth/auth0/callback" => "auth0#callback"
   get "/auth/failure" => "auth0#failure"
