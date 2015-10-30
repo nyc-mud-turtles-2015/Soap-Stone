ba5a0eda4ae13936646b1f1f6083ced9593d3494
Peter Rosal <peter.rosal@gmail.com>
2015-10-30 11:41:54 -0400

    Add relation and validation for drop photo and add s3 cred method

--- app/models/drop.rb
+++ app/models/drop.rb
@@ -4,11 +4,23 @@ class Drop < ActiveRecord::Base
   has_many :snaps
   validates :user, presence: true
   validate :has_some_content
+  has_attached_file :photo, styles: { medium: "640x640>", thumb: "292x292>" }, default_url: "/images/:style/missing.png"
+                    :storage => :s3, 
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
   def has_some_content
     unless photo || text
       errors.add(:drop, "you need something in here to drop some sick art")
     end
+  end
+
+  def s3_credentials
+    {
+      bucket: Rails.application.secrets.s3_bucket,
+      access_key_id: Rails.application.secrets.s3_key_id,
+      secret_access_key: Rails.application.secrets.s3_secret_key
+    }
   end
 
 end
