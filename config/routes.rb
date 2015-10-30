4f3f7fdfacffadf1692d07d9e775369d183aed30
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-30 00:24:15 -0400

    fix auth0 route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'maps#index'
-  
+
   resources :maps, only: [:index]
 
-  get '/login', :to => 'sessions#new', :as => 'login'
+  get "/login", :to => 'sessions#new', :as => 'login'
   get "/auth/auth0/callback" => "auth0#callback"
-  get "/auth/failure" => "auth#failure"
+  get "/auth/failure" => "auth0#failure"
 end
