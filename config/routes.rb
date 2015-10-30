a980b1e5f41a0d24996b6a9cac0818ac48011885
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-30 00:03:30 -0400

    add auth0 routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :maps, only: [:index]
 
   get '/login', :to => 'sessions#new', :as => 'login'
+  get "/auth/auth0/callback" => "auth0#callback"
+  get "/auth/failure" => "auth#failure"
 end
