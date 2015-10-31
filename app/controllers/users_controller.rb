5636ed035044fdd196d2b741a591d98a9c74860d
Peter Rosal <peter.rosal@gmail.com>
2015-10-30 20:24:12 -0400

    Update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:username, :uid)
-  end
+  
 end
