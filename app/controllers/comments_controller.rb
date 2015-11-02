fd7d207b4ef273591bc8c54d628f991f685574dd
spacebear5000 <david.giahuy.tu@gmail.com>
2015-11-01 23:28:35 -0500

    force login

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+  def created
+    if request.xhr?
+      drop = Drop.find(params[:comment_id])
+      comment = drop.comments.build(user: current_user)
+      if comment.save
+        render plain: {success: true}.to_json
+      else
+        render plain: {failure: drop.errors.full_messages.join(",")}.to_json, status: 500
+      end
+    end
+  end
+end
