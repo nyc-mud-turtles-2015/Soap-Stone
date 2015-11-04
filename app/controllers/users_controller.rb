0bd4de43394fc309205585744bfd39a60eae9804
Peter Rosal <peter.rosal@gmail.com>
2015-11-04 12:45:38 -0500

    Add edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id]) 
+    binding.pry
+    if @user.update(username: params[:user][:username])
+      redirect_to @user
+    else
+      flash.now.alert = @user.errors.full_messages.join(', ')
+      render plain: {failure: @user.errors.full_messages.join(",")}.to_json, status: 422
+    end
+  end
 end
